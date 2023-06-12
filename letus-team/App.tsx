830b79dee8c97f4b684b5912589d9c6fe1c450b5
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2023-06-12 14:26:57 -0300

    feat(navigation): added routes on the app file

--- letus-team/App.tsx
+++ letus-team/App.tsx
@@ -1,21 +1,19 @@
-/* import {Groups} from '@screens/Groups'; */
-/* import { NewGroup } from '@screens/NewGroup'; */
+
 import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
 import { useFonts,Roboto_400Regular,Roboto_700Bold} from '@expo-google-fonts/roboto'
 import theme from '@theme/index'
-import { NewPlayers } from '@screens/Players';
 import Loading from '@components/Loading';
+import { Router } from './src/routes';
 export default function App() {
   const [fontsLoader]=useFonts({Roboto_400Regular,Roboto_700Bold})
-
   return (
     <ThemeProvider theme={theme}>
       <StatusBar barStyle='light-content' backgroundColor={'transparent'}
       translucent/>
       {
       fontsLoader
-      ?<NewPlayers groupName='Exódia'/>/* <Groups/> */
+      ?<Router/>
       :<Loading/>
     }
     </ThemeProvider>
